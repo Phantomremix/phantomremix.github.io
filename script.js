38f8d646beef7c5b5daa1c8c74d803b7a53bab8e
Glitch (fan-living-dormouse) <none>
2021-12-07 18:46:58 +0000

    🏂🚥 Updated with Glitch

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-var distance = function() {
-  var x1 = parseFloat(document.getElementById("x1").value);
-  var y1 = parseFloat(document.getElementById("y1").value);
-  var x2 = parseFloat(document.getElementById("x2").value);
-  var y2 = parseFloat(document.getElementById("y2").value);
-
+var onConvertClick = function() {
+  var miles = document.getElementById("miles").value;
   var outputParagraph = document.getElementById("outputParagraph");
 
-  if (isNaN(x1) || isNaN(y1) || isNaN(x2) || isNaN(y2)) {
-    outputParagraph.textContent = "You entered bad input";
+  if (isNaN(miles)) {
+    outputParagraph.innerHTML = "Sorry, you didn't enter a number.";
   } else {
-    var d = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2).toFixed(2);
+    var kilo = 1.60934 * parseFloat(miles);
 
-    outputParagraph.textContent =
-      "The distance will show up here " + d + " units.";
+    outputParagraph.innerHTML = "Kilometers = " + kilo + " km.";
   }
 };
