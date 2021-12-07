cbf95f9bad80b4812b1ae3d842452635412a604a
Phantom <93003861+Phantomremix@users.noreply.github.com>
2021-12-07 14:00:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ var onConvertClick = function() {
     outputParagraph.innerHTML = "Kilometers = " + kilo + " km.";
   }
 };
+
+var distance = function() {
+  var x1 = parseFloat(document.getElementById("x1").value);
+  var y1 = parseFloat(document.getElementById("y1").value);
+  var x2 = parseFloat(document.getElementById("x2").value);
+  var y2 = parseFloat(document.getElementById("y2").value);
+
+  var outputParagraph = document.getElementById("outputParagraph");
+
+  if (isNaN(x1) || isNaN(y1) || isNaN(x2) || isNaN(y2)) {
+    outputParagraph.textContent = "You entered bad input";
+  } else {
+    var d = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+
+    outputParagraph.textContent =
+      "The distance will show up here " + d + " units.";
+  }
+};
