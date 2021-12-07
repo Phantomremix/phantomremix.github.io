248e413607f7935f023394a8e59c59a25befb9ed
Glitch (spotty-foam-trilby) <none>
2021-12-07 18:14:04 +0000

    💶🚇 Updated with Glitch

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-var onConvertClick = function() {
-  var miles = document.getElementById("miles").value;
+var distance = function() {
+  var x1 = parseFloat(document.getElementById("x1").value);
+  var y1 = parseFloat(document.getElementById("y1").value);
+  var x2 = parseFloat(document.getElementById("x2").value);
+  var y2 = parseFloat(document.getElementById("y2").value);
+
   var outputParagraph = document.getElementById("outputParagraph");
 
-  if (isNaN(miles)) {
-    outputParagraph.innerHTML = "Sorry, you didn't enter a number.";
+  if (isNaN(x1) || isNaN(y1) || isNaN(x2) || isNaN(y2)) {
+    outputParagraph.textContent = "You entered bad input";
   } else {
-    var kilo = 1.60934 * parseFloat(miles);
+    var d = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2).toFixed(2);
 
-    outputParagraph.innerHTML = "Kilometers = " + kilo + " km.";
+    outputParagraph.textContent =
+      "The distance will show up here " + d + " units.";
   }
 };
